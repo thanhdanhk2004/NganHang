d222bf82aa564bc11179941b7bc5d2ceeba68f7f
thanhdanhk2004 <121719416+thanhdanhk2004@users.noreply.github.com>
2024-01-02 15:27:33 +0700

    Sửa chức năng đọc file

--- NganHang/src/com/nhom/baitaplon/TaiKhoanCoKyHan.java
+++ NganHang/src/com/nhom/baitaplon/TaiKhoanCoKyHan.java
@@ -144,24 +144,4 @@ public class TaiKhoanCoKyHan extends TaiKhoanKhongKyHan {
         this.thongTinKyHan = thongTinKyHan;
     }
     
-    public TaiKhoanCoKyHan moTaiKhoanTuFile(String str[]){
-        TaiKhoanCoKyHan tkckh = new TaiKhoanCoKyHan();
-        int dem=0;
-        System.out.print(Arrays.toString(str));
-        tkckh.setHoTen(str[dem++].trim());
-        tkckh.setSoCCCD(str[dem++].trim());
-        tkckh.setNgaySinh(LocalDate.parse(str[dem++].trim(), DateTimeFormatter.ofPattern(CauHinh.DATE_FORMAT)));
-        tkckh.setQueQuan(str[dem++].trim());
-        tkckh.setGioiTinh(str[dem++].trim());
-        tkckh.setNgayDangKy(LocalDate.parse(str[++dem].trim(), DateTimeFormatter.ofPattern(CauHinh.DATE_FORMAT)));
-        tkckh.setSoTaiKhoan(str[++dem].trim());
-        tkckh.setMatKhau(Integer.parseInt(str[++dem].trim()));
-        tkckh.setSoTienGui(Double.parseDouble(str[++dem].trim()));
-        if (str[++dem].trim().equalsIgnoreCase("true"))
-            tkckh.setTrangThai(true);
-        else
-            tkckh.setTrangThai(false);
-        //tkckh.hienThi();
-        return tkckh;
-    }
 }
